4db1dc20989dfa0dc06ca2c80e612603dddda6ea
rickardsundin <rickard@snart.nu>
2013-10-21 17:12:11 +0200

    New message

--- src/main/java/se/citerus/labs/Main.java
+++ src/main/java/se/citerus/labs/Main.java
@@ -3,6 +3,6 @@ package se.citerus.labs;
 public class Main {
     public static void main(String[] args) {
         int lightId = 2;
-        new HueMorseCommunicator(lightId).send("tobias");
+        new HueMorseCommunicator(lightId).send("citerus");
     }
 }
